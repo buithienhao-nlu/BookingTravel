134f81632fdc1d768a2462abd460ec8ccc92a22e
buithienhao-nlu <18130069@st.hcmuaf.edu.vn>
2022-05-13 12:04:36 +0700

    Commit Home 3

---
+++ src/page/Contact/Contact.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Contact.css";
+
+export class Contact extends React.Component {
+  render() {
+    return <h1>Contact</h1>;
+  }
+}
--- src/page/Destination/Destination.js
+++ src/page/Destination/Destination.js
@@ -2,10 +2,10 @@ import * as React from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./About.css";
+import "./Destination.css";
 
-export class About extends React.Component {
+export class Destination extends React.Component {
   render() {
-    return <h1>About</h1>;
+    return <h1>Destination</h1>;
   }
 }
